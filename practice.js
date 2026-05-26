e0a00d2707a1dfafe28d8b8174424115cd7b2517
warabimochi <287585744+warabimochi-dev@users.noreply.github.com>
2026-05-26 14:21:17 +0900

    練習問題に挑戦

---
+++ practice.js
@@ -0,0 +1,39 @@
+const numbers = [5, 12, 21, 34, 73, 83, 90];
+
+const names = ["わらび", "もち", "きなこ", "さくら",]
+
+
+const sum = () => {
+    let sumNumbers = 0;
+   numbers.forEach ((num) => {
+    sumNumbers += num;
+   }); 
+    console.log(sumNumbers);
+}
+
+const multi = () => {
+    let sumNumbers = 0;
+    numbers.forEach((num) => {
+        sumNumbers += num*2
+    });
+    console.log(sumNumbers)
+}
+
+const findEven = () => {
+    numbers.forEach((num) => {
+        if(num % 2 === 0){
+            console.log(num);
+        }
+    });
+}
+
+const greet = () =>{
+    names.forEach((name) => {
+        console.log(`${name}さん、こんにちは`);
+    });
+}
+
+sum();
+multi();
+findEven();
+greet();
